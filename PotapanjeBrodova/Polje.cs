58da1a9760352949ba4d824de341d6487c24619d
julijan <julijan.sribar@gmail.com>
2016-06-29 21:39:26 +0200

    Dodaj klasu Polje i test za tu klasu

---
+++ PotapanjeBrodova/Polje.cs
@@ -0,0 +1,15 @@
+﻿// "Polje.cs" u projektu "PotapanjeBrodova"
+namespace PotapanjeBrodova
+{
+    public class Polje
+    {
+        public Polje(int redak, int stupac)
+        {
+            Redak = redak;
+            Stupac = stupac;
+        }
+
+        public readonly int Redak;
+        public readonly int Stupac;
+    }
+}
---
+++ TestPotapanjaBrodova/TestPolja.cs
@@ -0,0 +1,18 @@
+﻿// "TestPolja.cs" u projektu "TestPotapanjaBrodova"
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotapanjeBrodova;
+
+namespace TestPotapanjaBrodova
+{
+    [TestClass]
+    public class TestPolja
+    {
+        [TestMethod]
+        public void Polje_RedakIStupacJednakiSuArgumentimaKonstruktora()
+        {
+            Polje p = new Polje(1, 2);
+            Assert.AreEqual(1, p.Redak);
+            Assert.AreEqual(2, p.Stupac);
+        }
+    }
+}
