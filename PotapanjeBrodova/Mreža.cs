8227461cdc5660c4a4ea05169d0d9052e2ebebc9
julijan <julijan.sribar@gmail.com>
2016-06-29 22:00:02 +0200

    Dodaj klasu Mreža i testove za tu klasu.

---
+++ PotapanjeBrodova/Mreža.cs
@@ -0,0 +1,45 @@
+﻿// "Mreža.cs" u projektu "PotapanjeBrodova"
+using System;
+using System.Collections.Generic;
+
+namespace PotapanjeBrodova
+{
+    public class Mreža
+    {
+        public Mreža(int redaka, int stupaca)
+        {
+            Redaka = redaka;
+            Stupaca = stupaca;
+            polja = new Polje[Redaka, Stupaca];
+            for (int r = 0; r < Redaka; ++r)
+            {
+                for (int s = 0; s < Stupaca; ++s)
+                    polja[r, s] = new Polje(r, s);
+            }
+        }
+
+        public IEnumerable<Polje> DajRaspoloživaPolja()
+        {
+            List<Polje> raspoloživa = new List<Polje>();
+            for (int r = 0; r < Redaka; ++r)
+            {
+                for (int s = 0; s < Stupaca; ++s)
+                {
+                    if (polja[r, s] != null)
+                        raspoloživa.Add(polja[r, s]);
+                }
+            }
+            return raspoloživa;
+        }
+
+        public void EliminirajPolje(int redak, int stupac)
+        {
+            polja[redak, stupac] = null;
+        }
+
+        private Polje[,] polja;
+
+        public readonly int Redaka;
+        public readonly int Stupaca;
+    }
+}
---
+++ TestPotapanjaBrodova/TestMreže.cs
@@ -0,0 +1,35 @@
+﻿// "TestMreže.cs" u projektu "TestPotapanjaBrodova"
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotapanjeBrodova;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestPotapanjaBrodova
+{
+    [TestClass]
+    public class TestMreže
+    {
+        [TestMethod]
+        public void Mreža_DajRaspoloživaPolja_NaPočetkuVraćaSvaPolja()
+        {
+            Mreža m = new Mreža(5, 5);
+            IEnumerable<Polje> polja = m.DajRaspoloživaPolja();
+            Assert.AreEqual(25, polja.Count());
+            // provjeravamo postoji li (samo jedno) polje [0, 0]
+            Assert.AreEqual(1, polja.Count(p => { return p.Redak == 0 && p.Stupac == 0; }));
+            // provjeravamo postoji li (samo jedno) polje [4, 4]
+            Assert.AreEqual(1, polja.Count(p => { return p.Redak == 4 && p.Stupac == 4; }));
+        }
+
+        [TestMethod]
+        public void Mreža_DajRaspoloživaPolja_VraćaSamoNeeliminaranaPolja()
+        {
+            Mreža m = new Mreža(5, 5);
+            m.EliminirajPolje(1, 3);
+            IEnumerable<Polje> polja = m.DajRaspoloživaPolja();
+            Assert.AreEqual(24, polja.Count());
+            // provjeravamo da polje [1, 3] ne postoji u listi raspoloživih polja
+            Assert.AreEqual(0, polja.Count(p => { return p.Redak == 1 && p.Stupac == 3; }));
+        }
+    }
+}
