9735a9b70cd5a417fd40e4f0088b458ae5f9b32d
julijan <julijan.sribar@gmail.com>
2016-07-06 00:20:46 +0200

    Dodaj test flote i početnu implementaciju flote.

---
+++ PotapanjeBrodova/Flota.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotapanjeBrodova
+{
+public class Flota
+{
+    public void DodajBrod(IEnumerable<Polje> polja)
+    {
+        brodovi.Add(new Brod(polja));
+    }
+
+    public IEnumerable<Brod> Brodovi
+    {
+        get { return brodovi; }
+    }
+
+    private List<Brod> brodovi = new List<Brod>();
+}
+}
---
+++ TestPotapanjaBrodova/TestFlote.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotapanjeBrodova;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestPotapanjaBrodova
+{
+    [TestClass]
+    public class TestFlote
+    {
+        [TestMethod]
+        public void Flota_DodajBrod_DodajeBrodUFlotu()
+        {
+            Flota f = new Flota();
+            List<Polje> polja = new List<Polje>
+            {
+                new Polje(1, 2),
+                new Polje(1, 3),
+                new Polje(1, 4)
+            };
+            f.DodajBrod(polja);
+
+            Assert.AreEqual(1, f.Brodovi.Count());
+        }
+
+        [TestMethod]
+        public void Flota_DodajBrod_DvaPozivDodajuDvaBroda()
+        {
+            Flota f = new Flota();
+            List<Polje> polja = new List<Polje>
+            {
+                new Polje(1, 2),
+                new Polje(1, 3),
+                new Polje(1, 4)
+            };
+            f.DodajBrod(polja);
+
+            polja = new List<Polje>
+            {
+                new Polje(2, 7),
+                new Polje(3, 7),
+                new Polje(4, 7),
+                new Polje(5, 7)
+            };
+            f.DodajBrod(polja);
+
+            Assert.AreEqual(2, f.Brodovi.Count());
+        }
+    }
+}
