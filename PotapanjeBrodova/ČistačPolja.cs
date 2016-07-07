dfc6b9da456bf00255dd597342c6a44cb814c3fc
julijan <julijan.sribar@gmail.com>
2016-07-08 00:58:34 +0200

    Dodaj  test ČistačaPolja i početnu definiciju klase.

---
+++ PotapanjeBrodova/ČistačPolja.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotapanjeBrodova
+{
+    public class ČistačPolja
+    {
+        public ČistačPolja(Mreža mreža)
+        {
+            this.mreža = mreža;
+        }
+
+        public void Ukloni(IEnumerable<Polje> brodskaPolja)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Mreža mreža;
+    }
+}
---
+++ TestPotapanjaBrodova/TestČistačaPolja.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotapanjeBrodova;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestPotapanjaBrodova
+{
+    [TestClass]
+    public class TestČistačaPolja
+    {
+        [TestMethod]
+        public void ČistačPolja_UklanjaBrodskaIOkolnaPolja()
+        {
+            Mreža mreža = new Mreža(10, 10);
+            ČistačPolja e = new ČistačPolja(mreža);
+            List<Polje> brodskaPolja = new List<Polje>
+            {
+                new Polje(4, 3), new Polje(4, 4), new Polje(4, 5), new Polje(4, 6)
+            };
+            e.Ukloni(brodskaPolja);
+            IEnumerable<Polje> polja = mreža.DajRaspoloživaPolja();
+            Assert.AreEqual(82, polja.Count());
+            // prvo provjerimo brodska polja:
+            Assert.AreEqual(0, polja.Count(p => p.Redak == 4 && p.Stupac == 3));
+            Assert.AreEqual(0, polja.Count(p => p.Redak == 4 && p.Stupac == 4));
+            Assert.AreEqual(0, polja.Count(p => p.Redak == 4 && p.Stupac == 5));
+            Assert.AreEqual(0, polja.Count(p => p.Redak == 4 && p.Stupac == 6));
+            // potom provjerimo okolna polja (samo u uglovima):
+            Assert.AreEqual(0, polja.Count(p => p.Redak == 3 && p.Stupac == 2));
+            Assert.AreEqual(0, polja.Count(p => p.Redak == 3 && p.Stupac == 7));
+            Assert.AreEqual(0, polja.Count(p => p.Redak == 5 && p.Stupac == 2));
+            Assert.AreEqual(0, polja.Count(p => p.Redak == 5 && p.Stupac == 7));
+        }
+
+        [TestMethod]
+        public void ČistačPolja_UklanjaBrodskaIOkolnaPoljaZaBrodULijevomGornjemKutu()
+        {
+            Mreža mreža = new Mreža(10, 10);
+            ČistačPolja e = new ČistačPolja(mreža);
+            List<Polje> brodskaPolja = new List<Polje>
+            {
+                new Polje(4, 3), new Polje(4, 4), new Polje(4, 5), new Polje(4, 6)
+            };
+            e.Ukloni(brodskaPolja);
+            IEnumerable<Polje> polja = mreža.DajRaspoloživaPolja();
+            Assert.AreEqual(82, polja.Count());
+            // prvo provjerimo brodska polja:
+            Assert.AreEqual(0, polja.Count(p => p.Redak == 4 && p.Stupac == 3));
+            Assert.AreEqual(0, polja.Count(p => p.Redak == 4 && p.Stupac == 4));
+            Assert.AreEqual(0, polja.Count(p => p.Redak == 4 && p.Stupac == 5));
+            Assert.AreEqual(0, polja.Count(p => p.Redak == 4 && p.Stupac == 6));
+            // potom provjerimo okolna polja (samo u uglovima):
+            Assert.AreEqual(0, polja.Count(p => p.Redak == 3 && p.Stupac == 2));
+            Assert.AreEqual(0, polja.Count(p => p.Redak == 3 && p.Stupac == 7));
+            Assert.AreEqual(0, polja.Count(p => p.Redak == 5 && p.Stupac == 2));
+            Assert.AreEqual(0, polja.Count(p => p.Redak == 5 && p.Stupac == 7));
+        }
+    }
+}
