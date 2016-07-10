aff2bd2fa09657255579cf49902d8d8f488d7115
julijan <julijan.sribar@gmail.com>
2016-07-10 13:29:33 +0200

    Zamijeni metodu Distinct sa metodom All.

--- PotapanjeBrodova/Proširenja.cs
+++ PotapanjeBrodova/Proširenja.cs
@@ -7,39 +7,14 @@ namespace PotapanjeBrodova
 {
     public static class Proširenja
     {
-        private class VertikalnaPoravnatostPolja : IEqualityComparer<Polje>
-        {
-            public bool Equals(Polje x, Polje y)
-            {
-                return x.Stupac == y.Stupac;
-            }
-
-            public int GetHashCode(Polje obj)
-            {
-                return obj.Stupac.GetHashCode();
-            }
-        }
-
-        private class HorizontalnaPoravnatostPolja : IEqualityComparer<Polje>
-        {
-            public bool Equals(Polje x, Polje y)
-            {
-                return x.Redak == y.Redak;
-            }
-
-            public int GetHashCode(Polje obj)
-            {
-                return obj.Redak.GetHashCode();
-            }
-        }
-
         public static IEnumerable<Polje> Sortiraj(this IEnumerable<Polje> polja)
         {
             if (polja.Count() <= 1)
                 return polja;
-            bool horizontalnoPoravnata = polja.Distinct(new HorizontalnaPoravnatostPolja()).Count() == 1;
-            bool vertikalnoPoravnata = polja.Distinct(new VertikalnaPoravnatostPolja()).Count() == 1;
-            if (horizontalnoPoravnata == vertikalnoPoravnata)
+            Polje prvo = polja.First();
+            bool vodoravnoPoravnata = polja.Skip(1).All(p => p.Redak == prvo.Redak);
+            bool uspravnoPoravnata = polja.Skip(1).All(p => p.Stupac == prvo.Stupac);
+            if (vodoravnoPoravnata == uspravnoPoravnata)
                 throw new ArgumentException();
             return polja.OrderBy(p => p.Redak + p.Stupac);
         }
