bcddc4e92a15e6cb2678b36ef9f04210e1addb45
julijan <julijan.sribar@gmail.com>
2016-07-16 11:41:38 +0200

    Implementiraj obje metode Equals u klasi Polje.

--- PotapanjeBrodova/Polje.cs
+++ PotapanjeBrodova/Polje.cs
@@ -11,9 +11,23 @@ namespace PotapanjeBrodova
             Stupac = stupac;
         }
 
+        // tipski sigurna metoda Equals:
         public bool Equals(Polje drugo)
         {
+            if (drugo == null)
+                return false;
             return Redak == drugo.Redak && Stupac == drugo.Stupac;
+        }
+
+        // tipski nesigurna metoda Equals:
+        public override bool Equals(object drugi)
+        {
+            if (drugi == null)
+                return false;
+            if (GetType() != drugi.GetType())
+                return false;
+            // poziva tipski sigurnu izvedbu: 
+            return Equals((Polje)drugi);
         }
 
         public readonly int Redak;
