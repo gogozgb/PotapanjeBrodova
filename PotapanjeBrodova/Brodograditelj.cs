ba056a2a438c6d1570916bf0c2b5aabf10f0f183
julijan <julijan.sribar@gmail.com>
2016-07-17 23:00:36 +0200

    Implementiraj Brodograditelja.

--- PotapanjeBrodova/Brodograditelj.cs
+++ PotapanjeBrodova/Brodograditelj.cs
@@ -2,16 +2,55 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PotapanjeBrodova
 {
     public class Brodograditelj
     {
+        public Brodograditelj()
+        {
+            izbornik = new NasumičniIzbornikPolja();
+            čistač = new ČistačPolja();
+        }
+
         public Flota SložiFlotu(int redaka, int stupaca, IEnumerable<int> duljineBrodova)
         {
-            throw new NotImplementedException();
+            // za svaki slučaj sortiramo duljine od najveće prema namanjoj
+            duljineBrodova = duljineBrodova.OrderByDescending(d => d);
+            int brojPokušaja = 5;
+            for (int i = 0; i < brojPokušaja; ++i)
+            {
+                try
+                {
+                    Mreža m = new Mreža(redaka, stupaca);
+                    return SložiBrodove(m, duljineBrodova);
+                }
+                catch (ApplicationException)
+                {
+                }
+            }
+            var poruka = string.Format("Nije uspio složiti flotu ni nakon {0} pokušaja", brojPokušaja);
+            throw new ApplicationException(poruka);
         }
+
+        private Flota SložiBrodove(Mreža mreža, IEnumerable<int> duljineBrodova)
+        {
+            Flota f = new Flota();
+            foreach (int duljina in duljineBrodova)
+            {
+                var raspoloživa = mreža.RaspoloživaPolja;
+                var tražilica = new TražilicaNizovaPolja(raspoloživa);
+                var kandidati = tražilica.DajNizovePolja(duljina);
+                if (kandidati.Count() == 0)
+                    throw new ApplicationException();
+                var izbor = izbornik.Izaberi(kandidati);
+                f.DodajBrod(izbor);
+                čistač.Ukloni(mreža, izbor);
+            }
+            return f;
+        }
+
+        private NasumičniIzbornikPolja izbornik;
+        private ČistačPolja čistač;
     }
 }
