08764575b70b9650ec368c982bf844af9f59e83b
julijan <julijan.sribar@gmail.com>
2016-08-24 00:09:49 +0200

    Napiši početnu implementaciju i testove klase Napipač

---
+++ PotapanjeBrodova/Napipač.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace PotapanjeBrodova
+{
+    public class Napipač : IPucač
+    {
+        public Napipač(Mreža mreža, int duljinaBroda)
+        {
+            this.mreža = mreža;
+            this.duljinaBroda = duljinaBroda;
+            this.tražilica = new TražilicaNizovaPolja(mreža);
+        }
+
+        public Polje UputiPucanj()
+        {
+            var kandidati = DajKandidate();
+            int izbor = slučajni.Next(kandidati.Count());
+            zadnjeGađano = kandidati.ElementAt(izbor);
+            return zadnjeGađano;
+        }
+
+        public void EvidentirajRezultat(RezultatGađanja rezultat)
+        {
+            // Napipač smije evidentirati samo jedno polje:
+            Debug.Assert(pogođenaPolja.Count == 0);
+
+            if (rezultat != RezultatGađanja.Promašaj)
+            {
+                pogođenaPolja.Add(zadnjeGađano);
+            }
+            mreža.UkloniPolje(zadnjeGađano.Redak, zadnjeGađano.Stupac);
+        }
+
+        public IEnumerable<Polje> PogođenaPolja
+        {
+            get
+            {
+                return pogođenaPolja;
+            }
+        }
+
+        private IEnumerable<Polje> DajKandidate()
+        {
+            var svaPolja = tražilica.DajNizovePolja(duljinaBroda).SelectMany(p => p).ToList();
+            var sortiraneGrupe = svaPolja.GroupBy(polje => polje).OrderByDescending(grupa => grupa.Count());
+            return sortiraneGrupe.TakeWhile(grupa => grupa.Count() == sortiraneGrupe.First().Count()).Select(grupa => grupa.Key);
+        }
+
+        private Mreža mreža;
+        private int duljinaBroda;
+        private Polje zadnjeGađano;
+        private List<Polje> pogođenaPolja = new List<Polje>();
+        private TražilicaNizovaPolja tražilica;
+        private Random slučajni = new Random();
+    }
+}
---
+++ TestPotapanjaBrodova/TestNapipača.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotapanjeBrodova;
+
+namespace TestPotapanjaBrodova
+{
+    [TestClass]
+    public class TestNapipača
+    {
+        [TestMethod]
+        public void Napipač_UputiPucanj_VraćaPoljeKojeMožePripadatiNajvećemBrojuBrodovaDuljine3()
+        {
+            Mreža m = new Mreža(1, 5);
+            int duljinaBroda = 3;
+            Napipač n = new Napipač(m, duljinaBroda);
+            Polje p = n.UputiPucanj();
+            Assert.AreEqual(new Polje(0, 2), p);
+        }
+
+        [TestMethod]
+        public void Napipač_UputiPucanj_VraćaJednoOd3PoljaKojeMožePripadatiNajvećemBrojuBrodovaDuljine5()
+        {
+            Mreža m = new Mreža(1, 8);
+            int duljinaBroda = 5;
+            Napipač n = new Napipač(m, duljinaBroda);
+            Polje p = n.UputiPucanj();
+            Polje[] moguća = new Polje[] { new Polje(0, 3), new Polje(0, 4), new Polje(0, 5) };
+            Assert.IsTrue(moguća.Contains(p));
+        }
+
+        [TestMethod]
+        public void Napipač_EvidentirajRezultat_UklanjaGađanoPoljeIzMrežeZaPromašaj()
+        {
+            Mreža m = new Mreža(10, 10);
+            int duljinaBroda = 5;
+            Napipač n = new Napipač(m, duljinaBroda);
+            Polje gađanoPolje = n.UputiPucanj();
+            Assert.IsTrue(m.RaspoloživaPolja.Contains(gađanoPolje));
+            n.EvidentirajRezultat(RezultatGađanja.Promašaj);
+            Assert.IsFalse(m.RaspoloživaPolja.Contains(gađanoPolje));
+        }
+
+        [TestMethod]
+        public void Napipač_EvidentirajRezultat_UklanjaGađanoPoljeIzMrežeZaPogodak()
+        {
+            Mreža m = new Mreža(10, 10);
+            int duljinaBroda = 5;
+            Napipač n = new Napipač(m, duljinaBroda);
+            Polje gađanoPolje = n.UputiPucanj();
+            Assert.IsTrue(m.RaspoloživaPolja.Contains(gađanoPolje));
+            n.EvidentirajRezultat(RezultatGađanja.Pogodak);
+            Assert.IsFalse(m.RaspoloživaPolja.Contains(gađanoPolje));
+        }
+
+        [TestMethod]
+        public void Napipač_EvidentirajRezultat_UklanjaGađanoPoljeIzMrežeZaPotonuće()
+        {
+            Mreža m = new Mreža(10, 10);
+            int duljinaBroda = 5;
+            Napipač n = new Napipač(m, duljinaBroda);
+            Polje gađanoPolje = n.UputiPucanj();
+            Assert.IsTrue(m.RaspoloživaPolja.Contains(gađanoPolje));
+            n.EvidentirajRezultat(RezultatGađanja.Potonuće);
+            Assert.IsFalse(m.RaspoloživaPolja.Contains(gađanoPolje));
+        }
+
+        [TestMethod]
+        public void Napipač_EvidentirajRezultat_DodajePogađenoPoljeMeđuPogođenaPolja()
+        {
+            Mreža m = new Mreža(10, 10);
+            int duljinaBroda = 5;
+            Napipač n = new Napipač(m, duljinaBroda);
+            Polje gađanoPolje = n.UputiPucanj();
+            Assert.AreEqual(0, n.PogođenaPolja.Count());
+            n.EvidentirajRezultat(RezultatGađanja.Pogodak);
+            Assert.IsTrue(n.PogođenaPolja.Contains(gađanoPolje));
+        }
+    }
+}
