82a09181caf21400c46142610847c9052e25987f
julijan <julijan.sribar@gmail.com>
2016-09-11 23:21:33 +0200

    Dodana je klasa Tamanitelj i testovi za nju.

--- PotapanjeBrodova/Tamanitelj.cs
+++ PotapanjeBrodova/Tamanitelj.cs
@@ -2,19 +2,21 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace PotapanjeBrodova
 {
     using ListePolja = List<IEnumerable<Polje>>;
 
-    public class Kružitelj : IPucač
+    public class Tamanitelj : IPucač
     {
-        public Kružitelj(Mreža mreža, int duljinaBroda, Polje pogođenoPolje)
+        public Tamanitelj(Mreža mreža, int duljinaBroda, IEnumerable<Polje> pogođenaPolja)
         {
             this.mreža = mreža;
             this.duljinaBroda = duljinaBroda;
             this.tražilica = new TražilicaNizovaPolja(mreža);
-            pogođenaPolja.Add(pogođenoPolje);
+            this.pogođenaPolja = new List<Polje>(pogođenaPolja);
         }
 
         public Polje UputiPucanj()
@@ -31,7 +33,10 @@ namespace PotapanjeBrodova
         public void EvidentirajRezultat(RezultatGađanja rezultat)
         {
             if (rezultat != RezultatGađanja.Promašaj)
+            {
                 pogođenaPolja.Add(zadnjeGađano);
+                pogođenaPolja.Sortiraj();
+            }
             zadnjeGađano = null;
         }
 
@@ -46,26 +51,36 @@ namespace PotapanjeBrodova
         public IEnumerable<Polje> DajKandidate()
         {
             ListePolja nizoviPolja = new ListePolja();
-            foreach (Smjer smjer in Enum.GetValues(typeof(Smjer)))
-            {
-                var nizPolja = tražilica.DajPoljaUNastavku(pogođenaPolja.First(), smjer);
-                if (nizPolja.Count() >= 0)
-                    nizoviPolja.Add(nizPolja);
-            }
+            var smjerovi = DajSmjerove();
+
+            var nizPolja = tražilica.DajPoljaUNastavku(pogođenaPolja.First(), smjerovi.First());
+            if (nizPolja.Count() >= 0)
+                nizoviPolja.Add(nizPolja);
+            nizPolja = tražilica.DajPoljaUNastavku(pogođenaPolja.Last(), smjerovi.Last());
+            if (nizPolja.Count() >= 0)
+                nizoviPolja.Add(nizPolja);
+
             // traži najdulji niz
             int najdulji = nizoviPolja.Max(niz => niz.Count());
             // odabire samo nizove koji su duljine veće od duljine broda - 1
             // ili su najveće duljine
-            int granica = Math.Min(najdulji, duljinaBroda - 1);
+            int granica = Math.Min(najdulji, duljinaBroda - pogođenaPolja.Count);
             var probraniNizovi = nizoviPolja.Where(niz => niz.Count() >= granica);
             // vraća prve članove nizova, tj. polja uz prvo pogođeno
             return probraniNizovi.Select(niz => niz.First());
         }
 
+        private IEnumerable<Smjer> DajSmjerove()
+        {
+            if (pogođenaPolja.First().Redak == pogođenaPolja.Last().Redak)
+                return new List<Smjer>{ Smjer.Lijevo, Smjer.Desno };
+            return new List<Smjer> { Smjer.Gore, Smjer.Dolje };
+        }
+
         private readonly Mreža mreža;
         private readonly int duljinaBroda;
         private Polje zadnjeGađano;
-        private readonly List<Polje> pogođenaPolja = new List<Polje>();
+        private readonly List<Polje> pogođenaPolja;
         private readonly TražilicaNizovaPolja tražilica;
         private readonly Random slučajni = new Random();
     }
---
+++ TestPotapanjaBrodova/TestTamanitelja.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotapanjeBrodova;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestPotapanjaBrodova
+{
+    [TestClass]
+    public class TestTamanitelja
+    {
+        [TestMethod]
+        public void Tamanitelj_DajKandidate_ZaDvaPoljaUNastavkuVraćaObaPoljaAkoUNjihovimSmjerovimaImaVišePoljaOdPreostaleDuljineBroda()
+        {
+            Mreža mreža = new Mreža(10, 10);
+            int duljinaBroda = 3;
+            List<Polje> pogođenaPolja = new List<Polje> { new Polje(4, 3), new Polje(4, 4) };
+            Tamanitelj pucač = new Tamanitelj(mreža, duljinaBroda, pogođenaPolja);
+            IEnumerable<Polje> kandidati = pucač.DajKandidate();
+            Assert.AreEqual(2, kandidati.Count());
+            Assert.IsTrue(kandidati.Contains(new Polje(4, 2)));
+            Assert.IsTrue(kandidati.Contains(new Polje(4, 5)));
+        }
+
+        [TestMethod]
+        public void Tamanitelj_DajKandidate_ZaDvaPoljaUNastavkuVraćaSamoOnoUČijemSmjeruImaVišePoljaOdPreostaleDuljineBroda()
+        {
+            Mreža mreža = new Mreža(10, 10);
+            int duljinaBroda = 4;
+            List<Polje> pogođenaPolja = new List<Polje> { new Polje(4, 1), new Polje(4, 2) };
+            Tamanitelj pucač = new Tamanitelj(mreža, duljinaBroda, pogođenaPolja);
+            IEnumerable<Polje> kandidati = pucač.DajKandidate();
+            Assert.AreEqual(1, kandidati.Count());
+            Assert.IsTrue(kandidati.Contains(new Polje(4, 3)));
+        }
+
+        [TestMethod]
+        public void Tamanitelj_DajKandidate_ZaDvaPoljaUNastavkuVraćaObaPoljaAkoUNjihovimSmjerovimaImajednakoPoljaAliManjeOdPreostaleDuljineBroda()
+        {
+            Mreža mreža = new Mreža(6, 6);
+            int duljinaBroda = 5;
+            List<Polje> pogođenaPolja = new List<Polje> { new Polje(2, 4), new Polje(3, 4) };
+            Tamanitelj pucač = new Tamanitelj(mreža, duljinaBroda, pogođenaPolja);
+            IEnumerable<Polje> kandidati = pucač.DajKandidate();
+            Assert.AreEqual(2, kandidati.Count());
+            Assert.IsTrue(kandidati.Contains(new Polje(1, 4)));
+            Assert.IsTrue(kandidati.Contains(new Polje(4, 4)));
+        }
+
+        [TestMethod]
+        public void Tamanitelj_DajKandidate_ZaDvaPoljaUNastavkuVraćaOnoPoljeUČijemSmjeruImaVišePoljaAliManjeOdPreostaleDuljineBroda()
+        {
+            Mreža mreža = new Mreža(5, 5);
+            int duljinaBroda = 5;
+            List<Polje> pogođenaPolja = new List<Polje> { new Polje(2, 4), new Polje(3, 4) };
+            Tamanitelj pucač = new Tamanitelj(mreža, duljinaBroda, pogođenaPolja);
+            IEnumerable<Polje> kandidati = pucač.DajKandidate();
+            Assert.AreEqual(1, kandidati.Count());
+            Assert.IsTrue(kandidati.Contains(new Polje(1, 4)));
+        }
+    }
+}
