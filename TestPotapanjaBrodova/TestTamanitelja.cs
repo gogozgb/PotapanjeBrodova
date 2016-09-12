73df6d9c27c62c19041459b131f56628d76abccb
julijan <julijan.sribar@gmail.com>
2016-09-12 08:06:25 +0200

    Dodaj testove za Tamanitelja za brodove uz rub mreže,

--- TestPotapanjaBrodova/TestTamanitelja.cs
+++ TestPotapanjaBrodova/TestTamanitelja.cs
@@ -57,5 +57,53 @@ namespace TestPotapanjaBrodova
             Assert.AreEqual(1, kandidati.Count());
             Assert.IsTrue(kandidati.Contains(new Polje(1, 4)));
         }
+
+        [TestMethod]
+        public void Tamanitelj_DajKandidate_ZaBrodOdGornjegRubaMrežeVraćaPoljeIspod()
+        {
+            Mreža mreža = new Mreža(6, 6);
+            int duljinaBroda = 5;
+            List<Polje> pogođenaPolja = new List<Polje> { new Polje(2, 0), new Polje(2, 1) };
+            Tamanitelj pucač = new Tamanitelj(mreža, duljinaBroda, pogođenaPolja);
+            IEnumerable<Polje> kandidati = pucač.DajKandidate();
+            Assert.AreEqual(1, kandidati.Count());
+            Assert.IsTrue(kandidati.Contains(new Polje(2, 2)));
+        }
+
+        [TestMethod]
+        public void Tamanitelj_DajKandidate_ZaBrodOdDesnogRubaMrežeVraćaPoljeLijevo()
+        {
+            Mreža mreža = new Mreža(6, 6);
+            int duljinaBroda = 5;
+            List<Polje> pogođenaPolja = new List<Polje> { new Polje(4, 3), new Polje(5, 3) };
+            Tamanitelj pucač = new Tamanitelj(mreža, duljinaBroda, pogođenaPolja);
+            IEnumerable<Polje> kandidati = pucač.DajKandidate();
+            Assert.AreEqual(1, kandidati.Count());
+            Assert.IsTrue(kandidati.Contains(new Polje(3, 3)));
+        }
+
+        [TestMethod]
+        public void Tamanitelj_DajKandidate_ZaBrodOdDonjegRubaMrežeVraćaPoljeIznad()
+        {
+            Mreža mreža = new Mreža(6, 6);
+            int duljinaBroda = 5;
+            List<Polje> pogođenaPolja = new List<Polje> { new Polje(2, 4), new Polje(2, 5) };
+            Tamanitelj pucač = new Tamanitelj(mreža, duljinaBroda, pogođenaPolja);
+            IEnumerable<Polje> kandidati = pucač.DajKandidate();
+            Assert.AreEqual(1, kandidati.Count());
+            Assert.IsTrue(kandidati.Contains(new Polje(2, 3)));
+        }
+
+        [TestMethod]
+        public void Tamanitelj_DajKandidate_ZaBrodOdLijevogRubaMrežeVraćaPoljeDesno()
+        {
+            Mreža mreža = new Mreža(6, 6);
+            int duljinaBroda = 5;
+            List<Polje> pogođenaPolja = new List<Polje> { new Polje(0, 4), new Polje(1, 4) };
+            Tamanitelj pucač = new Tamanitelj(mreža, duljinaBroda, pogođenaPolja);
+            IEnumerable<Polje> kandidati = pucač.DajKandidate();
+            Assert.AreEqual(1, kandidati.Count());
+            Assert.IsTrue(kandidati.Contains(new Polje(2, 4)));
+        }
     }
 }
